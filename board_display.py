a852a91b9c1e138633ce9804d00d17ec9c1ec419
satyam9753 <satyam.anand9753@gmail.com>
2020-01-13 12:24:58 +0530

    1224

--- board_display.py
+++ board_display.py
@@ -9,8 +9,9 @@ sample_board = [
         [1, 2, 0, 0, 0, 7, 4, 0, 0],
         [0, 4, 9, 2, 0, 6, 0, 0, 7]
     ]
-def sudoko_board(b):
 
+def sudoko_board(b):
+	
     for i in range(len(b)):
 
         if i % 3 == 0 and i != 0:
@@ -19,12 +20,28 @@ def sudoko_board(b):
         for j in range(len(b[0])):
 
             if j % 3 == 0 and j != 0:
-                print(" | ", end="")
+                print(" | ", end = "")  # end="" is just to say that you want a space after the end of the statement instead of a new line character.
 
             if j == 8:
                 print(b[i][j])
 
             else:
-                print(str(b[i][j]) + " ", end="")
+                print(str(b[i][j]) + " ", end = "")
+            print(b[0] + "@")
 
 sudoko_board(sample_board)
+
+
+def empty_squares(b):   #finding empty squares in the given sudoko board
+
+	for i in range(len(b)):
+		for j in range(len(b[0])):
+			
+			if b[i][j] == 0:
+				return (i,j)  #ROW, COLUMN
+
+
+#def valid_board():
+
+
+
